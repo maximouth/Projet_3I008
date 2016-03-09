713cf1d4ca920afae297f560ffd14d6a281de0ea
Moi-meme <maximouth@linux.home>
2016-03-09 23:23:27 +0100

    noeud possible bon

--- holly_tree.ml
+++ holly_tree.ml
@@ -1,4 +1,24 @@
-type noeud = {C : int; F : int, suiv : holly_tree}
+type noeud = {c : int; f : int; suiv : holly_tree}
 	   and
-type holly_tree = noeud list;;
+	     holly_tree = noeud list;;
 
+
+  
+let rec noeud_l l f c n =
+  match n with
+  | 12 -> List.rev l
+  | z ->
+     (match abs(f - n) with
+	0 | 2 | 4 | 5 | 7 ->
+			 (match abs(c - n) with 
+			    0 | 1 | 2 | 3 | 4 | 5 | 7 ->
+						     noeud_l ({c = n ;
+							       f = f ;
+							       suiv = []}::l) f c (n + 1)
+			    | n -> noeud_l  l f c (n + 1))
+	| z -> noeud_l l f c (n + 1))
+;;
+
+let noeud_possible f c =
+  noeud_l [] f c 4
+;;	  
