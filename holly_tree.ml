725316500076e5b1e73fa178ecf7570f1c0d3103
Moi-meme <maximouth@linux.home>
2016-03-13 17:21:36 +0100

    quelqueq modifications a faire pour avoir un truc bien

--- holly_tree.ml
+++ holly_tree.ml
@@ -47,6 +47,49 @@ let arbre_possible (l : int list) : holly_tree =
   (arbre_p (noeud_possible  0 (List.hd l)) (List.tl l@(0::[])))
 ;;
 
-  arbre_possible [0;3];;
+
+let rec nettoyage (prec : noeud) (cour : holly_tree) (n : int) : holly_tree =
+  match cour with
+  | [] -> []
+  | {c = a ; f = b; suiv = []}::l' ->
+     (if (n = 0) then
+	{c = a ; f = b; suiv = []}::(nettoyage prec (List.tl cour) n)
+      else
+	(nettoyage prec (List.tl cour) n)
+     )
+  | {c = a ; f = b; suiv = s}::l' ->
+     {c = a ; f = b; suiv = (nettoyage {c = a ; f = b; suiv = s} s (n - 1))}::(nettoyage prec (List.tl cour) n)
+;;
+
+let rec nettoyer_arbre (abr : holly_tree) (n : int)  : holly_tree =
+  match abr with
+  | [] -> []
+  | {c = a ; f = b ; suiv = s}::abr' ->
+     {c = a ; f = b ; suiv = (nettoyage {c = a ; f = b ; suiv = s} s n )}::(nettoyer_arbre abr' n)
+;;
+    
+
+let rec alea (abr : holly_tree) (res : int list) (n : int) =
+  match abr with
+  | [] -> List.rev res
+  | [a] -> (List.rev (a.c::res))
+  | a::abr' ->
+     (if n < 1 then
+	(alea a.suiv (a.c::res) (Random.int (List.length a.suiv - 1)))
+      else
+	(alea abr' res (n - 1))
+     )
+;;
+		
+  
+let chant_alea (list : int list) : int list =  
+(*  let abr = (nettoyer_arbre (arbre_possible list) ((List.length list) -1) ) in
+ *)
+  let abr = arbre_possible list in
+  (alea abr [] (List.length abr))
+;;
+
+  chant_alea [0;3;0];;
+
 
   
